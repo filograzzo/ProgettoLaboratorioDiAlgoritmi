28e2aeda1a66a1deb2950346537afa7c5a020710
Filippo Grazzini <filippomaurograzzini@gmail.com>
2023-10-12 11:16:56 +0200

    Cancellate completamente le classi e le sto rifacendo da capo. Cominciato a fare la classe BinaryTreeClass.py in cui ho creato una classe TreeNode e una BinaryTree.
    TreeNode si può solo inizializzare e ha attributi che sono jey, p, right e left.
    
    BinaryTree ha metodi di ricerca di una chiave e di massimo e minimo oltre che un metodo per ritornare un array di tutti i numeri in ordine che poi verranno stampati nel main.
    Inizializzati brevemente anche i metodi per inserimento e cancellazione.

--- src/BinaryTreeClass.py
+++ src/BinaryTreeClass.py
@@ -1,31 +1,73 @@
 class TreeNode:
     def __init__(self, key):
         self.key = key
-        self.right = None
-        self.left = None
+        self.p = None  # puntatore al padre
+        self.right = None  # puntatore al figlio destro
+        self.left = None  # puntatore al figlio sinistro
 
 
 class BinaryTree:
     def __init__(self):
         self.root = None
 
-    def insert_recursive(self, root, key):
-        if root is None:
-            return TreeNode(key)
-        if key < root.key:
-            root.left = self._insert_recursive(root.left, key)
-        if key > root.key:
-            root.right = self._insert_recursive(root.right, key)
-        return root
+# TODO: metodi per inserìmento
+    @staticmethod
+    def recursive_insert(current, key):
+        if current is None:
 
     def insert(self, key):
-        self.root = self.insert_recursive(self.root, key)
+        recursive_insert(self.root, key)
 
-    def print_tree(self):
-        self._print_inorder(self.root)
+# TODO: metodi per cancellazione
+    @staticmethod
+    def recursive_delete(current, key):
+        if current.key == key:
 
-    def _print_inorder(self, node):
-        if node is not None:
-            self._print_inorder(node.left)
-            print(node.key, end=' ')
-            self._print_inorder(node.right)
+
+    def delete(self, key):  # deve ritornare un booleano per dire se ha effettivamente cancellato o non ha trovato il numero da cancellare
+        return recursive_delete(self.root, key)
+
+# metodi di ricerca ci mette O(h) con h altezza dell'albero di ricerca
+    @staticmethod
+    def recursive_search(current, key):
+        if current.key == key  or current is None:
+            return current  # ritorno il nodo che ha il valore uguale a quello che sto cercando oppure None se sono arrivato lla fine dell'albero senza trovare nulla
+        if current.key >= key:
+            recursive_search(current.left, key)
+        else:
+            recursive_search(current.right, key)
+
+    def search(self, key):
+        return recursive_search(self.root, key)
+
+# metodi per ricerca di minimo e massimo O(h)
+    @staticmethod
+    def iterative_maximum(current):
+        if current.right is None:
+            return current
+        iterative_maximum(current.right)
+
+    def find_maximum(self):
+        return iterative_maximum(self.root)
+
+    @staticmethod
+    def iterative_minimum(current):
+        if current.left is None:
+            return current
+        iterative_minimum(current.left)
+
+    def find_minimum(self):
+        return iterative_minimum(self.root)
+
+# metodi per ritornare un array di interi per poi stamparli nel main. Occupa Θ(n) dato che si chima due volte per nodo.
+    def tree_walk(self):
+        result = []
+        recursive_treewalk(self.root)
+        return result
+
+    @staticmethod
+    def recursive_treewalk(current):
+        if current is not None:
+            recursive_treewalk(current.left)
+            result.append(current.key)
+            recursive_treewalk(current.right)
